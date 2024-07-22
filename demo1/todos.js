0c7aa327218dfe349e2a5d6a6dca653c71b8efe6
Shako <shakoinbox@gmail.com>
2024-07-22 05:54:39 -0700

    comm5

--- demo1/todos.js
+++ demo1/todos.js
@@ -1,22 +1,18 @@
-// (1)-create application state: massive that holds list of (3) actions (e.g. - TODO - items)
-const todo = [
-    {description:'Walk dog', done: false},
-    {description:'Water the plants', done:false},
-    {description:'Sand th chairs', done:false}
-    ];
+// State of the app
+const todos = ['Walk the dog', 'Water the plants', 'Sand the chairs']
 
-// (2)- set variables to (HTML)-element referances getting (id) - selectors
-const addTodoInput = document.getElementById('todo-input');
-const addTodoButton = document.getElementById('add-todo-btn');
-const todoList = document.getElementById('todo-list');
+// HTML element references
+const addTodoInput = document.getElementById('todo-input')
+const addTodoButton = document.getElementById('add-todo-btn')
+const todosList = document.getElementById('todos-list')
 
-// (3) Initialize the view
-for(const todo of todos){
-    todoList.append(renderTodoInReadMode(todo));
+// Initialize the view
+for (const todo of todos) {
+  todosList.append(renderTodoInReadMode(todo))
 }
-// button disabled until (input) has at list 3 characters 
-addTodoInput.addEventListener('input', ()=>{
-    addTodoButton.ariaDisabled = addTodoInput.ariaValueMax.length < 3;
+
+addTodoInput.addEventListener('input', () => {
+  addTodoButton.disabled = addTodoInput.value.length < 3
 })
 
 addTodoInput.addEventListener('keydown', ({ key }) => {
@@ -25,31 +21,85 @@ addTodoInput.addEventListener('keydown', ({ key }) => {
   }
 })
 
-addTodoButton.addEventListener('click' , ()=>{
-    addTodo();
-});
+addTodoButton.addEventListener('click', () => {
+  addTodo()
+})
 
-// functions
-function renderTodoInReadMode(todo){
-    const li = document.createElement('li');
-    const span = document.createElement('span')
-    span.textContent = todo
-    span.addEventListener('dblclick', ()=>{
-        const idx = todos.indexOf(todo)
+// Functions
+function renderTodoInReadMode(todo) {
+  const li = document.createElement('li')
 
-        todoList.replaceChild(
-            renderTodoInReadMode(todo),
-            todoList.childNodes(idx)
-        )
-    })
-    li.append(span);
+  const span = document.createElement('span')
+  span.textContent = todo
+  span.addEventListener('dblclick', () => {
+    const idx = todos.indexOf(todo)
 
-    const button = document.createElement('button')
-    button.textContent = 'Done'
-    button.addEventListener('click', ()=>{
-        const idx = todos.indexOf(todo);
-    })
+    todosList.replaceChild(
+      renderTodoInEditMode(todo),
+      todosList.childNodes[idx]
+    )
+  })
+  li.append(span)
+
+  const button = document.createElement('button')
+  button.textContent = 'Done'
+  button.addEventListener('click', () => {
+    const idx = todos.indexOf(todo)
+    removeTodo(idx)
+  })
+  li.append(button)
+
+  return li
 }
-function addTodo(){
 
+function renderTodoInEditMode(todo) {
+  const li = document.createElement('li')
+
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.value = todo
+  li.append(input)
+
+  const saveBtn = document.createElement('button')
+  saveBtn.textContent = 'Save'
+  saveBtn.addEventListener('click', () => {
+    const idx = todos.indexOf(todo)
+    updateTodo(idx, input.value)
+  })
+  li.append(saveBtn)
+
+  const cancelBtn = document.createElement('button')
+  cancelBtn.textContent = 'Cancel'
+  cancelBtn.addEventListener('click', () => {
+    const idx = todos.indexOf(todo)
+    todosList.replaceChild(
+      renderTodoInReadMode(todo),
+      todosList.childNodes[idx]
+    )
+  })
+  li.append(cancelBtn)
+
+  return li
+}
+
+function addTodo() {
+  const description = addTodoInput.value
+
+  todos.push(description)
+  const todo = renderTodoInReadMode(description)
+  todosList.append(todo)
+
+  addTodoInput.value = ''
+  addTodoButton.disabled = true
+}
+
+function removeTodo(index) {
+  todos.splice(index, 1)
+  todosList.childNodes[index].remove()
+}
+
+function updateTodo(index, description) {
+  todos[index] = description
+  const todo = renderTodoInReadMode(description)
+  todosList.replaceChild(todo, todosList.childNodes[index])
 }
