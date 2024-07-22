8bc3da855348684e57f91a545e2bf5c27155f0f2
Shako <shakoinbox@gmail.com>
2024-07-21 23:12:17 -0700

    comm4

--- demo1/todos.js
+++ demo1/todos.js
@@ -1,1 +1,7 @@
-// hello javascript
+// create application state
+const todo = ['Walk dog', 'Water the plants', 'Sand th chairs'];
+
+// Html element referances by id here
+const addTodoInput = document.getElementById('todo-input');
+const addTodoButton = document.getElementById('add-todo-btn');
+const todoList = document.getElementById('todo-list');
